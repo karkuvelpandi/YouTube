423f4c9cb723158a3e26fe8dae934a51d28d63b9
Karkuvel Pandi P <110515092+karkuvelpandi@users.noreply.github.com>
2023-11-07 18:28:04 +0530

    Update api.js

--- youtube-clone/src/api.js
+++ youtube-clone/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3/",
   params: {
-    key: "AIzaSyCuTlxKBACdsHwG821C3HziAuVbduStBP8",
+    key: process.env.REACT_APP_YT_API,
   },
 });
 
